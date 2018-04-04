85fa75c65c37422b88b0c1cf2c47cb4724e612b4
rafaelpereira <rafaelpereira@192.168.0.12>
2018-04-04 11:18:29 -0300

    Exercícios sobre Cadastro de Cidades

---
+++ ExeProgComercial/src/Cidades/Arquivo.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Cidades;
+
+/**
+ *
+ * @author rafaelpereira
+ */
+
+import java.io.*;
+
+public class Arquivo {
+
+    private String nomeArquivo;
+    private BufferedWriter bw;
+    private BufferedReader br;
+    private File arq;
+
+    public Arquivo(String nomeArquivo) {
+        this.nomeArquivo = nomeArquivo;
+        this.arq = new File(this.nomeArquivo + ".txt");
+    }
+
+    public void gravar(String texto, boolean novo) {
+        try {
+            if (novo) {
+                bw = new BufferedWriter(new FileWriter(this.arq));
+            } else {
+                bw = new BufferedWriter(new FileWriter(this.arq, true));
+            }
+            if (!texto.equals("")) {
+                bw.write(texto);
+                bw.newLine();
+            }
+            bw.close();
+        } catch (Exception e) {
+        }
+    }
+
+    public String ler() {
+        String conteudo = null;
+        String texto = "";
+        try {
+            br = new BufferedReader(new FileReader(this.arq));
+            while ((conteudo = br.readLine()) != null) {
+                texto = texto + conteudo + "\n";
+            }
+            br.close();
+        } catch (Exception e) {
+        }
+        return texto;
+    }
+}
+
